c980d224dc2b5cb2626f74c448ddb37e0c099b38
Christos <khristos@lycos.com>
2018-09-14 23:41:17 -0400

    Add Player controls

---
+++ src/assets/js/component/efm_player.js
@@ -0,0 +1,7 @@
+//https://developer.mozilla.org/en-US/docs/Web/Accessibility/ARIA/ARIA_Techniques/Using_the_slider_role#Possible_effects_on_user_agents_and_assistive_technology
+
+function outputUpdate(vol) {
+  document.querySelector('#player__time-elapsed').value = vol;
+}
+
+
